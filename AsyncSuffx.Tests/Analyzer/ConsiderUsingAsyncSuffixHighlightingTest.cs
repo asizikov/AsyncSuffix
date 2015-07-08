1c4d08f116bb88f1e38325a1bfc71bfee9ced985
Anton <sizikov.anton@gmail.com>
2015-07-08 22:22:35 +0200

    unit tests project added

---
+++ AsyncSuffx.Tests/Analyzer/ConsiderUsingAsyncSuffixHighlightingTest.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.FeaturesTestFramework.Daemon;
+using JetBrains.ReSharper.Psi;
+using NUnit.Framework;
+using Sizikov.AsyncSuffix.Analyzer;
+
+namespace AsyncSuffx.Tests.Analyzer
+{
+    [TestFixture]
+    public class ConsiderUsingAsyncSuffixHighlightingTest : CSharpHighlightingTestBase
+    {
+        protected override bool HighlightingPredicate(IHighlighting highlighting, IPsiSourceFile sourceFile)
+
+        {
+            return highlighting is ConsiderUsingAsyncSuffixHighlighting;
+        }
+
+        protected override string RelativeTestDataPath
+        {
+            get { return @"Daemon\ConsiderUsingConfigureAwait"; }
+        }
+
+        [Test]
+        [TestCase("Case1.cs")]
+        public void Test(string testName)
+        {
+            DoTestSolution(testName);
+        }
+    }
+}
