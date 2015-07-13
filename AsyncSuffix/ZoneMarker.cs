9de64f34c955df76becce81829d47cde2122b16e
Anton Sizikov <anton.sizikov@albumprinter.com>
2015-07-13 16:33:30 +0200

    empy zone marker

--- AsyncSuffix/ZoneMarker.cs
+++ AsyncSuffix/ZoneMarker.cs
@@ -1,11 +1,9 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.ReSharper.Feature.Services;
-using JetBrains.ReSharper.Psi.CSharp;
 
 namespace Sizikov.AsyncSuffix
 {
     [ZoneMarker]
-    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>
+    public class ZoneMarker // : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<DaemonZone>
     {
     }
 }
