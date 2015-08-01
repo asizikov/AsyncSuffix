ec5bb4259f54db496f181109c5f25a186142d554
Anton <sizikov.anton@gmail.com>
2015-08-01 13:16:41 +0200

    ZoneMarker changed to more specific

--- AsyncSuffix/ZoneMarker.cs
+++ AsyncSuffix/ZoneMarker.cs
@@ -1,9 +1,12 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.CSharp;
 
 namespace Sizikov.AsyncSuffix
 {
     [ZoneMarker]
-    public class ZoneMarker // : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<DaemonZone>
+    public class ZoneMarker  : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<DaemonZone>
     {
     }
 }
