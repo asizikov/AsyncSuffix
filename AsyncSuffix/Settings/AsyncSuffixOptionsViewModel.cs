e04a3c178be524f3f1f8ab59614ebbf97d2c4d52
Anton <sizikov.anton@gmail.com>
2015-10-11 23:28:02 +0200

    #2 custom async types support. started

--- AsyncSuffix/Settings/AsyncSuffixOptionsViewModel.cs
+++ AsyncSuffix/Settings/AsyncSuffixOptionsViewModel.cs
@@ -15,12 +15,17 @@ namespace Sizikov.AsyncSuffix.Settings
         {
             SettingsStore = settings;
             ExcludeTestMethodsFromAnalysis = new Property<bool>(lifetime, "ExcludeTestMethodsFromAnalysis");
+            CustomAsyncTypesEnabled = new Property<bool>(lifetime, "CustomAsyncTypesEnabled");
             settings.SetBinding(lifetime, AsyncSuffixSettingsAccessor.ExcludeTestMethodsFromAnalysis,
                 ExcludeTestMethodsFromAnalysis);
+            settings.SetBinding(lifetime, AsyncSuffixSettingsAccessor.CustomAsyncTypesEnabled, CustomAsyncTypesEnabled);
         }
 
 
         [NotNull]
         public IProperty<bool> ExcludeTestMethodsFromAnalysis { get; private set; }
+
+        [NotNull]
+        public IProperty<bool> CustomAsyncTypesEnabled { get; private set; }
     }
 }
--- AsyncSuffix/Settings/AsyncSuffixSettings.cs
+++ AsyncSuffix/Settings/AsyncSuffixSettings.cs
@@ -1,10 +1,18 @@
 ﻿using JetBrains.Application.Settings;
+using JetBrains.Application.Settings.Store;
+using JetBrains.Metadata.Reader.API;
 
 namespace Sizikov.AsyncSuffix.Settings
 {
     [SettingsKey(typeof (EnvironmentSettings), "AsyncSuffix plugin settings")]
     public sealed class AsyncSuffixSettings
     {
-        [SettingsEntry(true, "Exclude test methods from analysis")] public bool ExcludeTestMethodsFromAnalysis;
+        [SettingsEntry(true, "Exclude test methods from analysis")]
+        public bool ExcludeTestMethodsFromAnalysis;
+        [SettingsEntry(false, "Apply analysis to custom types")]
+        public bool CustomAsyncTypesEnabled { get; set; }
+
+        [SettingsIndexedEntry("Custom types list")]
+        public IIndexedEntry<IClrTypeName,bool> CustomAsyncTypes { get; set; }
     }
 }
