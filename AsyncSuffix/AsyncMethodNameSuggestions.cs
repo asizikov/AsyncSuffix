fb39de382f8f67d324ee9429481dbd6ef60f622c
Anton Sizikov <sizikov.anton@gmail.com>
2015-11-09 08:50:38 +0100

    AsyncMethodNameSuggestions started

---
+++ AsyncSuffix/AsyncMethodNameSuggestions.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sizikov.AsyncSuffix
+{
+    public class AsyncMethodNameSuggestions
+    {
+        public IEnumerable<string> Get(IMethodDeclaration method)
+        {
+            if (!method.IsValid())
+            {
+                return Enumerable.Empty<string>();
+            }
+            var declared = method.DeclaredElement;
+            if (declared != null)
+            {
+                var newName = declared.ShortName + "Async";
+                return new List<string> { newName };
+            }
+            return Enumerable.Empty<string>();
+        }
+    }
+}
