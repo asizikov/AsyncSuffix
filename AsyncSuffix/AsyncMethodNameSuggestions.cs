78651dbaabec0d696b361a485e16dfe3e3ded10e
Anton Sizikov <sizikov.anton@gmail.com>
2015-11-15 17:34:13 +0100

    Take typos into account when generating suggestions

--- AsyncSuffix/AsyncMethodNameSuggestions.cs
+++ AsyncSuffix/AsyncMethodNameSuggestions.cs
@@ -1,27 +1,46 @@
-﻿using JetBrains.ReSharper.Psi;
+﻿using JetBrains.Annotations;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using JetBrains.Util;
+using JetBrains.Util.Extension;
 
 namespace Sizikov.AsyncSuffix
 {
     public class AsyncMethodNameSuggestions
     {
-        public IEnumerable<string> Get(IMethodDeclaration method)
+        private const string Async = "Async";
+        private static readonly List<string> typos = new List<string>
+                {
+                    "Asyn", "Asinc", "Asin", "async", "asyn", "asinc"
+                };
+
+        [NotNull]
+        public static List<string> Get([NotNull] IMethodDeclaration method)
         {
+            if (method == null) throw new ArgumentNullException("method");
             if (!method.IsValid())
             {
-                return Enumerable.Empty<string>();
+                return (List<string>) EmptyList<string>.InstanceList;
             }
             var declared = method.DeclaredElement;
             if (declared != null)
             {
-                var newName = declared.ShortName + "Async";
-                return new List<string> { newName };
+                var shortName = declared.ShortName;
+                var newName = shortName + Async;
+                var names = new List<string> { newName };
+                
+                foreach (var pattern in typos)
+                {
+                    if (shortName.EndsWith(pattern))
+                    {
+                        var result = shortName.SubstringBefore(pattern) + Async;
+                        names.Add(result);
+                    }
+                }
+                return names;
             }
-            return Enumerable.Empty<string>();
+            return (List<string>)EmptyList<string>.InstanceList;
         }
     }
 }
