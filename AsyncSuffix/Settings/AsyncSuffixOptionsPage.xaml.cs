5bbe3a4d55e12dd069b054c55f2bd990476963ea
Anton Sizikov <sizikov.anton@gmail.com>
2015-11-23 23:43:50 +0100

    :sparkles: Options page is deriving from CustomSimpleOptionsPage now

--- AsyncSuffix/Settings/AsyncSuffixOptionsPage.xaml.cs
+++ AsyncSuffix/Settings/AsyncSuffixOptionsPage.xaml.cs
@@ -4,27 +4,23 @@ using JetBrains.ReSharper.Feature.Services.Resources;
 using JetBrains.UI.CrossFramework;
 using JetBrains.UI.Options;
 using JetBrains.UI.Options.OptionPages;
+using JetBrains.UI.Options.OptionsDialog2.SimpleOptions;
 
 namespace Sizikov.AsyncSuffix.Settings
 {
-    [OptionsPage(PID, "AsyncSuffix", typeof(ServicesThemedIcons.AnalyzeThis),
+    [OptionsPage(PageId, "AsyncSuffix", typeof(ServicesThemedIcons.AnalyzeThis),
      ParentId = EnvironmentPage.Pid)]
-    public sealed partial class AsyncSuffixOptionsPage : IOptionsPage
+    public sealed partial class AsyncSuffixOptionsPage : CustomSimpleOptionsPage
     {
-        // ReSharper disable once InconsistentNaming
-        private const string PID = "AsyncSuffix";
+        private const string PageId = "AsyncSuffix";
 
         public AsyncSuffixOptionsPage([NotNull] Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store
-                                  )
+                                  ) : base(lifetime, store)
         {
-            InitializeComponent();
-            DataContext = new AsyncSuffixOptionsViewModel(lifetime, store);
-            Control = this;
+            AddHeader("Options");
+            AddBoolOption(
+                (AsyncSuffixSettings options) => options.ExcludeTestMethodsFromAnalysis,
+                "Exclude test methods from analysis");
         }
-
-        public EitherControl Control { get; private set; }
-        public string Id { get { return PID; } }
-        public bool OnOk() { return true; }
-        public bool ValidatePage() { return true; }
     }
 }
