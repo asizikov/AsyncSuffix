230cb1c3d9575052c16ec7b3826eab9b347a995a
Anton Sizikov <sizikov.anton@gmail.com>
2015-11-29 20:46:47 +0100

    Options page with StringCollectionEditViewModel, and do not try publish on PullRequest

--- AsyncSuffix/Settings/AsyncSuffixOptionsPage.xaml.cs
+++ AsyncSuffix/Settings/AsyncSuffixOptionsPage.xaml.cs
@@ -1,10 +1,17 @@
 ﻿using JetBrains.Annotations;
+using JetBrains.Application.Interop.NativeHook;
 using JetBrains.DataFlow;
 using JetBrains.ReSharper.Feature.Services.Resources;
+using JetBrains.UI.Application;
 using JetBrains.UI.CrossFramework;
 using JetBrains.UI.Options;
 using JetBrains.UI.Options.OptionPages;
 using JetBrains.UI.Options.OptionsDialog2.SimpleOptions;
+using JetBrains.UI.Resources;
+using JetBrains.UI.Validation;
+using JetBrains.UI.Wpf.Controls.StringCollectionEdit.Impl;
+using JetBrains.UI.Wpf.Controls.StringCollectionEdit.Impl.Buttons;
+using JetBrains.UI.Wpf.Controls.StringCollectionEdit.Impl.Items;
 
 namespace Sizikov.AsyncSuffix.Settings
 {
@@ -14,13 +21,19 @@ namespace Sizikov.AsyncSuffix.Settings
     {
         private const string PageId = "AsyncSuffix";
 
-        public AsyncSuffixOptionsPage([NotNull] Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store
-                                  ) : base(lifetime, store)
+        public AsyncSuffixOptionsPage([NotNull] Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store,
+                                 IWindowsHookManager windowsHookManager, FormValidators formValidators, IUIApplication iuiApplication) : base(lifetime, store)
         {
             AddHeader("Options");
             AddBoolOption(
                 (AsyncSuffixSettings options) => options.ExcludeTestMethodsFromAnalysis,
                 "Exclude test methods from analysis");
+            var editItemViewModelFactory = new DefaultCollectionEditItemViewModelFactory(null);
+            var buttonProviderFactory = new DefaultButtonProviderFactory(lifetime, windowsHookManager, formValidators, iuiApplication, editItemViewModelFactory);
+            var customAsyncTypes = new StringCollectionEditViewModel(lifetime, "Treat these types as async", buttonProviderFactory, editItemViewModelFactory);
+
+            AddCustomOption(customAsyncTypes);
+            
         }
     }
 }
