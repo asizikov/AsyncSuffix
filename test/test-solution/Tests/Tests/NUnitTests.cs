2499cfcf7568e7b1266490fc840935654589ef49
Anton Sizikov <sizikov.anton@gmail.com>
2016-02-14 19:06:44 +0100

    test solution added

---
+++ test/test-solution/Tests/Tests/NUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class NUnitTests
+    {
+        [Test]
+        public Task MyMethod()
+        {
+            return Task.FromResult<object>(null);
+        }
+
+        [TestCase(1)]
+        public Task TestCase(int x)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ test/test-solution/Tests/Tests/XUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class XUnitTests
+    {
+        [Fact]
+        public Task MyMethod()
+        {
+            return Task.FromResult<object>(null);
+        }
+
+        [Theory]
+        public Task TestCase(int x)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
