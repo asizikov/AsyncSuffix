67490830ba41ce60e2a03479d0bf83df2f9549f1
Anton Sizikov <sizikov.anton@gmail.com>
2016-09-04 10:49:26 +0200

    update pathe to solution

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 "tools\nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-"tools\nuget\NuGet.exe" restore ./src/MrEric.sln
+"tools\nuget\NuGet.exe" restore ./src/AsyncSuffix.sln
 "packages\FAKE\tools\Fake.exe" build.fsx
